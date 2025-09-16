90e1c4c923b712c39d61a8b8a57e0afdc452d7e3
Lucas Manera <lucas_emiliano_manera@hotmail.com>
2025-09-15 22:28:11 -0300

    actualizo estilos y formularios

--- Pag-python/app.py
+++ Pag-python/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import pymysql
 
 app = Flask(__name__)
+
+usuarios = []
 
 # Función para conectar a MySQL con PyMySQL
 def get_connection():
@@ -21,7 +23,8 @@ def get_connection():
         print(f"❌ Error al conectar con MySQL: {err}")
         return None
 
-# Ruta principal
+
+# Ruta principal (registro)
 @app.route("/", methods=["GET", "POST"])
 def register():
     mensaje = ""
@@ -38,6 +41,7 @@ def register():
                     cursor.execute(query, (nombre, email, password))
                     conn.commit()
                 mensaje = "✅ Usuario registrado correctamente!"
+                return redirect(url_for("logear"))
             except pymysql.MySQLError as err:
                 mensaje = f"❌ Error al insertar en MySQL: {err}"
             finally:
@@ -46,6 +50,48 @@ def register():
             mensaje = "❌ Error de conexión con la base de datos."
 
     return render_template("register.html", mensaje=mensaje)
+
+
+# Login
+@app.route("/login", methods=["GET", "POST"])
+def logear():
+    mensaje = ""
+    if request.method == "POST":
+        nombre = request.form.get("Nombre")
+        password = request.form.get("password")
+
+        conn = get_connection()
+        if conn:
+            try:
+                with conn.cursor() as cursor:
+                    query = "SELECT * FROM usuarios WHERE Nombre=%s AND Contraseña=%s"
+                    cursor.execute(query, (nombre, password))
+                    user = cursor.fetchone()
+
+                if user:
+                    usuarios.append(nombre)
+                    mensaje = f"✅ Bienvenido {nombre}!"
+                    return redirect(url_for("logueado"))
+                else:
+                    mensaje = "❌ Usuario o contraseña incorrectos."
+            except pymysql.MySQLError as err:
+                mensaje = f"❌ Error en MySQL: {err}"
+            finally:
+                conn.close()
+        else:
+            mensaje = "❌ Error de conexión con la base de datos."
+
+    return render_template("login.html", mensaje=mensaje)
+
+
+# Home (si está logueado)
+@app.route("/home")
+def logueado():
+    if usuarios:  # si hay alguien logueado
+        return render_template("index.html")
+    else:
+        return redirect(url_for("register"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3030, debug=True)
