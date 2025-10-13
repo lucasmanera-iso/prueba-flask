9711ba48145f46df90945a9c4887fc57ac952c9f
Lucas Manera <lucas_emiliano_manera@hotmail.com>
2025-10-13 19:53:55 -0300

    actualizacion grande

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from dotenv import load_dotenv
+import cloudinary
+import cloudinary.uploader
 import pymysql
 
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET", "devkey")
+
+# Config Cloudinary
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
 
 usuarios = []
 
 # Función para conectar a MySQL con PyMySQL
 def get_connection():
-    try:
-        connection = pymysql.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="1233",
-            database="catalogo",
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        print("✅ Conectado correctamente a MySQL con PyMySQL")
-        return connection
-    except pymysql.MySQLError as err:
-        print(f"❌ Error al conectar con MySQL: {err}")
-        return None
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")),
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 
 # Ruta principal (registro)
@@ -44,7 +54,7 @@ def register():
                         cursor.execute(query, (nombre, apellido,telefono, email, psw))
                         conn.commit()
                     mensaje = "✅ Usuario registrado correctamente!"
-                    return redirect(url_for("/"))
+                    return redirect("/")
                 except pymysql.MySQLError as err:
                     mensaje = f"❌ Error al insertar en MySQL: {err}"
                 finally:
@@ -72,9 +82,8 @@ def logear():
                     query = "SELECT * FROM usuario_admin WHERE Email=%s AND Psw=%s"
                     cursor.execute(query, (Email, password))
                     user = cursor.fetchone()
-
                 if user:
-                    usuarios.append(Email)
+                    usuarios.append(user)
                     return redirect(url_for("logueado"))
                 else:
                     mensaje = "❌ Usuario o contraseña incorrectos."
@@ -108,7 +117,7 @@ def administradores():
     return render_template("administradores.html", administradores=administradores)
 
 
-@app.route("/editar/<int:id>", methods=["GET", "POST"])
+@app.route("/editara/<int:id>", methods=["GET", "POST"])
 def editar_admin(id):
     if not usuarios:
         return redirect(url_for("logear"))
@@ -148,8 +157,75 @@ def editar_admin(id):
             print(f"❌ Error en MySQL: {err}")
         finally:
             conn.close()
+        
+        usuario = next((u for u in usuarios if u["id_usuario"] == id), None)
 
-    return render_template("editar.html", admin=admin)
+    return render_template("editara.html", usuario=usuario)
+
+@app.route("/editarp/<int:id>", methods=["GET", "POST"])
+def editar_producto(id):
+    if not usuarios:
+        return redirect(url_for("logear"))
+
+    conn = get_connection()
+    if request.method == "POST":
+        descripcion = request.form.get("descripcion")
+        precio = request.form.get("precio")
+        contenido = request.form.get("contenido")
+        stock = request.form.get("stock")
+        file = request.files.get("imagen")
+
+        imagen_url = None
+        if file and file.filename != "":
+            # Subir imagen nueva a Cloudinary
+            resultado = cloudinary.uploader.upload(file)
+            imagen_url = resultado.get("secure_url")
+
+        if conn:
+            try:
+                with conn.cursor() as cursor:
+                    if imagen_url:
+                        # Si hay nueva imagen
+                        query = """
+                            UPDATE productos
+                            SET descripcion=%s, precio=%s, contenido=%s, stock=%s, imagen=%s
+                            WHERE id_producto=%s
+                        """
+                        cursor.execute(query, (descripcion, precio, contenido, stock, imagen_url, id))
+                        conn.commit()
+
+                        return redirect("/productos")
+                    else:
+                        # Si no se subió imagen, no actualizar ese campo
+                        query = """
+                            UPDATE productos
+                            SET descripcion=%s, precio=%s, contenido=%s, stock=%s
+                            WHERE id_producto=%s
+                        """
+                        cursor.execute(query, (descripcion, precio, contenido, stock, id))
+                        conn.commit()
+
+                        return redirect("/productos")
+            except pymysql.MySQLError as err:
+                print(f"❌ Error al actualizar: {err}")
+            finally:
+                conn.close()
+
+        return redirect("productos.html")
+
+    # si es GET → mostrar datos actuales
+    if conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM productos WHERE id_producto=%s", (id,))
+                producto = cursor.fetchone()
+        except pymysql.MySQLError as err:
+            print(f"❌ Error en MySQL: {err}")
+        finally:
+            conn.close()
+        
+
+    return render_template("editarp.html", producto=producto)
 
 @app.route("/productos")
 def catalogo():
@@ -164,8 +240,52 @@ def catalogo():
             print(f"Error en MySQL: {err}")
         finally:
             conn.close()
-    return render_template("productos.html", productos=productos)
+    return render_template("productos.html", productos=productos, usuarios=usuarios)
 
+@app.route("/subir", methods=["GET", "POST"])
+def subir():
+    if usuarios:  # validación de login
+        if request.method == "POST":
+            nombre = request.form.get("nombre")
+            marca = request.form.get("marca")
+            descripcion = request.form.get("descripcion")
+            precio = request.form.get("precio")
+            contenido = request.form.get("contenido")
+            stock = request.form.get("stock")
+            file = request.files.get("imagen")
+            id_categoria = request.form.get("categoria")
+            id_usuario = usuarios[0]["id_usuario"]
+
+
+            try:
+                # Subir imagen a Cloudinary
+                resultado = cloudinary.uploader.upload(file)
+                imagen_url = resultado.get("secure_url")
+
+                # Guardar en la base de datos
+                conn = get_connection()
+                with conn.cursor() as cursor:
+                    query = """
+                        INSERT INTO productos 
+                        (nombre, marca, descripcion, precio, contenido, stock, imagen, id_categoria, id_usuario) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """
+                    cursor.execute(query, (nombre, marca, descripcion, precio, contenido, stock, imagen_url, id_categoria, id_usuario))
+                    conn.commit()
+                    print(nombre, marca, descripcion, precio, contenido, stock, imagen_url, id_categoria, id_usuario)
+
+                conn.close()
+                flash("✅ Producto subido correctamente")
+                return redirect(url_for("catalogo"))
+
+            except Exception as e:
+                app.logger.exception("Error subiendo producto")
+                flash(f"❌ Error: {e}")
+                return redirect(request.url)
+
+        return render_template("subir.html")
+
+    return redirect(url_for("logear"))
 
 
 # Home (si está logueado)
@@ -174,7 +294,7 @@ def logueado():
     
 
 
-    return render_template("index.html", usuarios=usuarios)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
